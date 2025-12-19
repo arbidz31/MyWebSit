ee845aba203301b9f300f5dd2c42086eb19fab9e
arbidz31 <Arbidz338@gmail.com>
2025-12-19 17:28:04 +0100

    Enhance hamburger menu and add toggle buttons
    Added functionality to toggle content display with buttons.

--- arbi.js
+++ arbi.js
@@ -1,6 +1,24 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 
+
 hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
+  navLinks.classList.toggle('active');
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const buttons = document.querySelectorAll(".bidaa-toggle-btn");
+
+    buttons.forEach(btn => {
+        btn.addEventListener("click", () => {
+            const content = btn.nextElementSibling;
+            if (content.style.display === "none" || content.style.display === "") {
+                content.style.display = "block";
+                btn.innerText = btn.innerText.replace("▼", "▲");
+            } else {
+                content.style.display = "none";
+                btn.innerText = btn.innerText.replace("▲", "▼");
+            }
+        });
+    });
 });
